35883cb3a9d14ff93ae42553f5062f446d7ddb92
urielhgb7-arch <urielhgb7@gmail.com>
2026-08-11 09:23:16 +0100

    Modif db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const { Pool } = require('pg');
-require('dotenv').config({ path: __dirname + '/../.env' });
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
